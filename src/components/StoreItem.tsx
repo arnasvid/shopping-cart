65fceafef74dd707149559ef24707732fb3ac93d
arnasvid <arnas.vidziunas@gmail.com>
2022-11-19 00:26:52 +0200

    Add functionality into store items, shopping-cart button now also takes the quantity from store

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrency";
 
 type StoreItemProps = {
@@ -9,7 +10,8 @@ type StoreItemProps = {
 };
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
-  const quantity = 0;
+    const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
+    const quantity = getItemQuantity(id);
 
   return (
     <Card className="h-100">
@@ -26,7 +28,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         </Card.Title>
         <div className="mt-auto">
           {quantity === 0 ? (
-            <button className="btn btn-primary w-100">+ Add To Cart</button>
+            <button className="btn btn-primary w-100" onClick={() => increaseCartQuantity(id)}>+ Add To Cart</button>
           ) : (
             <div
               className="d-flex align-items-center flex-column"
@@ -36,13 +38,13 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
                 className="d-flex align-items-center justify-content-center"
                 style={{ gap: ".5rem" }}
               >
-                <Button>-</Button>
+                <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
                 <div>
                     <span className="fs-3">{quantity}</span> in cart
                 </div>
-                <Button>+</Button>
+                <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
-              <Button variant="danger" size="sm">Remove</Button>
+              <Button variant="danger" size="sm" onClick={() => removeFromCart(id)}>Remove</Button>
             </div>
           )}
         </div>
